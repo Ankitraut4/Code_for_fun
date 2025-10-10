e2558790f736512522e486c0e332ed8a8d89f339
Ankit Raut <ankitraut9421@gmail.com>
2025-10-10 12:00:00 -0400

    Add folder: Insertion_Sort

---
+++ Insertion_Sort/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int[] arr={5,3,4,1,2};
+      Insertion_Sort(arr);
+        System.out.println(Arrays.toString(arr));
+     }
+
+    private static void Insertion_Sort(int[] arr){
+        for (int i=0;i< arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if (arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }else {
+                    break;
+                }
+            }
+        }
+    }
+}
