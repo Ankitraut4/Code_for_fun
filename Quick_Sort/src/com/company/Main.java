9a0bb3532ec050ba22fb27aaf46a2531b77634a8
Ankit Raut <ankitraut9421@gmail.com>
2025-10-14 12:00:00 -0400

    Add folder: Quick_Sort

---
+++ Quick_Sort/src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr={5,2,4,3,1};
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static void quickSort(int[] arr,int low,int high){
+        if (low>=high){
+            return;
+        }
+        int start=low;
+        int end=high;
+
+        int mid=start+(end-start)/2;
+
+        int pivot=arr[mid];
+
+        while (start<=end) {
+            while (arr[start] < pivot) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+            //our pivot for first loop is at  correct index
+            //sort in similar way for lrft and right part
+            quickSort(arr,low,end);
+            quickSort(arr,start,high);
+
+    }
+}
