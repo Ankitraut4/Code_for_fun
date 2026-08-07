2d4344ad85185050bce8e7a8270f3aa517a205fd
Ankit <92351539+Ankitraut4@users.noreply.github.com>
2026-08-07 13:24:45 -0400

    Time: 14 ms (47.46%), Space: 46.3 MB (80.7%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -20,12 +20,7 @@ class Solution {
         return maxCount;
         
     }
-    public boolean isVowel(char c){
-        switch(c){
-            case 'a' : case 'e' : case 'i' : case 'o' : case 'u':
-            return true;
-            default:
-            return false;
-        }
+    private boolean isVowel(char ch){
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ; 
     }
-}
+    }
