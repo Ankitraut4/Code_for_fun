550ca8d0c0833bd4383477997a28b33fa53a4d7e
Ankit <92351539+Ankitraut4@users.noreply.github.com>
2026-08-09 15:51:47 -0400

    Time: 9 ms (99.75%), Space: 48.1 MB (61.68%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,3 +1,5 @@
+//Practical  efficiency = Very Efficient, time complexity = O(n) , Space = O(n)
+//because this approach is essentially a manually implemented stack.
 class Solution {
     public String removeStars(String s) {
         int top = -1;
@@ -13,4 +15,46 @@ class Solution {
         }
         return new String(charArray, 0, top+1);
     }
+//Practical efficiency = Better, but time complexity = O(n) , Space = O(n)
+//     class Solution {
+//     public String removeStars(String s) {
+//         StringBuilder sb = new StringBuilder();
+
+//         for(char ch : s.toCharArray()) {
+//             if(ch == '*') {
+//                 sb.deleteCharAt(sb.length() - 1);
+//             } else {
+//                 sb.append(ch);
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+//Practical efficiency = Good but time complexity = O(n) , Space = O(n)
+// class Solution {
+//     public String removeStars(String s) {
+
+//         Stack<Character> stack = new Stack<>();
+
+//         for(char ch : s.toCharArray()) {
+
+//             if(ch == '*') {
+//                 stack.pop();
+//             } else {
+//                 stack.push(ch);
+//             }
+//         }
+
+//         StringBuilder result = new StringBuilder();
+
+//         for(char ch : stack) {
+//             result.append(ch);
+//         }
+
+//         return result.toString();
+//     }
+// }
+
 }
