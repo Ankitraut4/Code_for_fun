8c72db1b50ed56590a5bb3d3f27d49f516a26b43
Ankit <92351539+Ankitraut4@users.noreply.github.com>
2026-08-09 17:15:50 -0400

    Time: 1 ms (100%), Space: 46.7 MB (84.09%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,63 +1,27 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-    int[] arr =new int[asteroids.length];
+        int[] arr=new int[asteroids.length];
+        int top = -1;
+        for(int a : asteroids){
+            boolean remove = false;
+            while(top>=0 && arr[top]>0 && a<0){
+                if(arr[top]< -a){
+                    top--;
+                }else if(arr[top]==-a){
+                    top--;
+                    remove = true;
+                    break;
+                }else{
+                    remove = true;
+                    break;
+                }
 
-    int top=-1;
-    for(int n : asteroids){
-        boolean remove =false;
-
-        while(top>=0 && arr[top]>0 && n<0){
-            if(arr[top]<-n){
-                top--;
-            }else if(arr[top]== -n){
-                top--;
-                remove=true;
-                break;
-            }else{
-                remove=true;
-                break;
             }
-
-        }
-        if(!remove){
-            arr[++top]=n;
-        }
-
         
-    }
-    
-    return Arrays.copyOf(arr, top + 1);
-    
-    
-    
-    
-    
-    
-    //    Stack<Integer> stack=new Stack<>();
-
-    //    for(int n : asteroids){
-    //     boolean remove= false;
-
-    //     while(!stack.isEmpty() && stack.peek()>0 && n < 0){
-    //         if(stack.peek() < -n){
-    //             stack.pop();
-    //         }else if(stack.peek()== -n){
-    //             stack.pop();
-    //             remove=true;
-    //             break;
-    //         }else{
-    //             remove=true;
-    //             break;
-    //         }
-    //     }
-    //     if(!remove){
-    //         stack.push(n);
-    //     }
-    //    }
-    //    int[] arr=new int[stack.size()];
-    //    for(int i=stack.size()-1;i>=0;i--){
-    //         arr[i]=stack.pop();
-    //    }
-    //    return arr;
+        if(!remove){
+            arr[++top]=a;
+        }
+        }
+        return Arrays.copyOf(arr, top+1);
     }
 }
