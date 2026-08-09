c20ec1caac14b77ea6278895c2d2331ee0b63a41
Ankit <92351539+Ankitraut4@users.noreply.github.com>
2026-08-09 17:25:26 -0400

    Time: 1 ms (100%), Space: 46.9 MB (72.88%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -25,3 +25,42 @@ class Solution {
         return Arrays.copyOf(arr, top+1);
     }
 }
+// class Solution {
+//     public int[] asteroidCollision(int[] asteroids) {
+
+//         Stack<Integer> stack = new Stack<>();
+
+//         for (int n : asteroids) {
+
+//             boolean remove = false;
+
+//             while (!stack.isEmpty() && stack.peek() > 0 && n < 0) {
+
+//                 if (stack.peek() < -n) {
+//                     stack.pop();
+
+//                 } else if (stack.peek() == -n) {
+//                     stack.pop();
+//                     remove = true;
+//                     break;
+
+//                 } else {
+//                     remove = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!remove) {
+//                 stack.push(n);
+//             }
+//         }
+
+//         int[] result = new int[stack.size()];
+
+//         for (int i = 0; i < stack.size(); i++) {
+//             result[i] = stack.get(i);
+//         }
+
+//         return result;
+//     }
+// }
